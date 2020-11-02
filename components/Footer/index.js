7c0ab21aaf686a173a428382c79f85fff92c2684
Jeferson Leskevicius <jeferson.leskevicius@gmail.com>
2020-11-02 16:27:55 -0300

    Correção dos links

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,9 +4,9 @@ const Footer = () => {
   return (
     <div className='bg-gray-700 p-4'>
       <div className='container mx-auto text-center font-bold text-white'>
-        Projeto desenvolvido por: <a href='#' className='hover:underline'>Jeferson Leskevicius</a> / {' '}
-        <a href='#' className='hover:underline'>Linkedin</a> / {' '}
-        <a href='#' className='hover:underline'>Github</a>
+        Projeto desenvolvido por: <a href='https://vercel.com/jefersonleskevicius' className='hover:underline'>Jeferson Leskevicius</a> / {' '}
+        <a href='https://www.linkedin.com/in/jefersonleskevicius' className='hover:underline'>Linkedin</a> / {' '}
+        <a href='https://github.com/jefersonleskevicius' className='hover:underline'>Github</a>
         <div className='mt-4'>
           <img className='inline p-4' src='/logo_semana_fsm.png' />
           <img className='inline p-4' src='/logo_devpleno.png' />
